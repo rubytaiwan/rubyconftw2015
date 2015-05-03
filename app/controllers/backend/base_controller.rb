71707e5dad7fb03e4d4206477bbfd359ec95873e
Awaru Ryudo <ryudoawaru@gmail.com>
2015-05-03 16:20:08 +0800

    將 Backend::BaseController 設計成共用 controller

--- app/controllers/backend/base_controller.rb
+++ app/controllers/backend/base_controller.rb
@@ -1,9 +1,81 @@
 class Backend::BaseController < ApplicationController
   layout 'backend'
+  helper_method :current_collection, :current_object
 
-  # before_action :authenticate_user!
+  def index
+  end
 
-  # rescue_from CanCan::AccessDenied do |exception|
-  #   redirect_to root_url, :alert => exception.message
-  # end
+  def new
+    @current_object = collection_scope.new
+  end
+
+  def create
+    @current_object = collection_scope.create(object_params)
+    respond_to do |f|
+      f.html do
+        flash[:success] = t('flash.successfully_created')
+        redirect_to url_after_create
+      end
+      f.json
+    end
+  end
+
+  def destroy
+    current_object.destroy
+    respond_to do |f|
+      f.html do
+        flash[:success] = t('flash.successfully_destroyed')
+        redirect_to url_after_destroy
+      end
+      f.json
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if current_object.update(object_params)
+      respond_to do |f|
+        f.html do
+          flash[:success] = t('flash.successfully_updated')
+          redirect_to url_after_update
+        end
+        f.json
+      end
+    else
+      respond_to do |f|
+        f.html do |f|
+          render action: :edit
+        end
+        f.json
+      end
+    end
+  end
+
+  private
+
+  def url_after_create
+    request.env['HTTP_REFERER'] || url_for(action: :index)
+  end
+
+  alias url_after_update url_after_create
+  alias url_after_destroy url_after_create
+
+  def collection_scope
+    # You should implement it in your controller
+  end
+
+  def object_params
+    # You should implement it in your controller
+  end
+
+  def current_collection
+    @collection ||= collection_scope.page(params[:page])
+  end
+
+  def current_object
+    @current_object ||= collection_scope.find(params[:id])
+  end
+
 end
--- app/controllers/backend/speakers_controller.rb
+++ app/controllers/backend/speakers_controller.rb
@@ -1,54 +1,60 @@
 class Backend::SpeakersController < Backend::BaseController
-  before_action :find_speaker, only:[:show, :destroy, :edit, :update]
-  
-  def index
-  	@speakers = Speaker.all
-  end
+#  before_action :find_speaker, only:[:show, :destroy, :edit, :update]
 
-  def new
-  	@speaker = Speaker.new
-  end
-
-  def create
-  	@speaker = Speaker.new(speaker_params)
-  	if @speaker.save
-  		flash[:success] = "新增成功"
-  		redirect_to backend_speakers_path
-  	else
-      render 'new'
-    end
-  	
-  end
-
-  def show
-  	
-  end
-
-  def destroy
-  	
-  	@speaker.destroy
-  	redirect_to backend_speakers_path
-  end
-
-  def edit
-  	
-  end
-  def update
-  	
-  	if @speaker.update(speaker_params)
-      redirect_to backend_speakers_path
-    else
-      render :edit
-    end
-  end
+#  def index
+#  	@speakers = Speaker.all
+#  end
+#
+#  def new
+#  	@speaker = Speaker.new
+#  end
+#
+#  def create
+#  	@speaker = Speaker.new(speaker_params)
+#  	if @speaker.save
+#  		flash[:success] = "新增成功"
+#  		redirect_to backend_speakers_path
+#  	else
+#      render 'new'
+#    end
+#
+#  end
+#
+#  def show
+#
+#  end
+#
+#  def destroy
+#
+#  	@speaker.destroy
+#  	redirect_to backend_speakers_path
+#  end
+#
+#  def edit
+#
+#  end
+#  def update
+#
+#  	if @speaker.update(speaker_params)
+#      redirect_to backend_speakers_path
+#    else
+#      render :edit
+#    end
+#  end
 
   private
-  	def find_speaker
-  		@speaker = Speaker.find(params[:id])
+
+    def collection_scope
+      Speaker
+    end
+
+#  	def find_speaker
+#  		@speaker = Speaker.find(params[:id])
+#  	end
+
+  	def object_params
+      params.require(:speaker).permit(:name, :avatat, :biography , :subject , :asbtract)
   	end
 
-  	def speaker_params
-    params.require(:speaker).permit(:name, :avatat, :biography , :subject , :asbtract)
-  	end
 end
 
