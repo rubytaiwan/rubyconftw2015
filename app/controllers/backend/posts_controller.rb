7a25d0e137f14df71d4043529b401e23114dff5a
Nina Yeh <ninayeh1211@gmail.com>
2015-05-04 15:27:18 +0800

    使用共用controller

--- app/controllers/backend/posts_controller.rb
+++ app/controllers/backend/posts_controller.rb
@@ -1,48 +1,51 @@
 class Backend::PostsController < Backend::BaseController
-  before_action :find_post, only:[:show,:edit,:destroy,:update]
-  def index
-  	@posts = Post.all
-  end
+  # before_action :find_post, only:[:show,:edit,:destroy,:update]
+  # def index
+  # 	@posts = Post.all
+  # end
 
-  def new
-  	@post = Post.new
-  end
+  # def new
+  # 	@post = Post.new
+  # end
 
-  def create
-  	@post = Post.new(post_params)
-  	if @post.save
-  		redirect_to backend_posts_path
-  	else
-  		render 'new'
-  	end
-  end
+  # def create
+  # 	@post = Post.new(post_params)
+  # 	if @post.save
+  # 		redirect_to backend_posts_path
+  # 	else
+  # 		render 'new'
+  # 	end
+  # end
 
-  def show
+  # def show
   	
-  end
+  # end
 
-  def edit
+  # def edit
   	
-  end
+  # end
 
-  def update
-  	if @post.update(post_params)
-  		redirect_to backend_posts_path
-  	else
-  		render :edit
-  	end
-  end
+  # def update
+  # 	if @post.update(post_params)
+  # 		redirect_to backend_posts_path
+  # 	else
+  # 		render :edit
+  # 	end
+  # end
 
-  def destroy
-  	@post.destroy
-  		redirect_to backend_posts_path
-  end
+  # def destroy
+  # 	@post.destroy
+  # 		redirect_to backend_posts_path
+  # end
 
   private
-  	def find_post
-  		@post = Post.find(params[:id])
-  	end
-  	def post_params
+    def collection_scope
+      Post
+    end
+  	# def find_post
+  	# 	@post = Post.find(params[:id])
+  	# end
+  	def object_params
   		params.require(:post).permit(:title,:content,:publish_date)
   	end
 end
