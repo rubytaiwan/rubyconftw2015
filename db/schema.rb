55b2e7c85ab215b6fb7f09d346d046df9de48bc7
Awaru Ryudo <ryudoawaru@gmail.com>
2015-06-30 18:13:44 +0800

    1. 把 NAV 的 LI 做成 partial 2. Sponsor Link 加 target blank

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150630080250) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
@@ -29,14 +32,6 @@ ActiveRecord::Schema.define(version: 20150630080250) do
     t.text     "abstract"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "sponsers", force: :cascade do |t|
-    t.string   "name"
-    t.string   "image"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
   end
 
   create_table "sponsors", force: :cascade do |t|
@@ -56,6 +51,6 @@ ActiveRecord::Schema.define(version: 20150630080250) do
     t.string   "password_digest"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
