5896ac69581a3fb7da383e1aa5fc1a5ebf60c5f3
Awaru Ryudo <ryudoawaru@gmail.com>
2015-07-12 11:02:20 +0800

    讓 MOBILE / DESKTOP 用同樣的menuitem

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,5 +2,6 @@ class StaticPagesController < ApplicationController
   def home
   	@speakers = Speaker.order(:sort_order).all
   	@sponsors = Sponsor.order("level, name").all
+    @nav_sections = %w{home speaker schedule venue coc sponsor}
   end
 end
