3721ecdd107f758cd7dcd56275c742e636e3eab8
Awaru Ryudo <ryudoawaru@gmail.com>
2015-07-13 17:09:48 +0800

    加入 Sponsor的等級 group_by

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
   	@speakers = Speaker.order(:sort_order).all
-  	@sponsors = Sponsor.order("level, name").all
+  	@sponsors_hash = Sponsor.all.to_a.group_by{|s| s.level}
     @nav_sections = %w{home speaker schedule venue coc sponsor}
   end
 end
