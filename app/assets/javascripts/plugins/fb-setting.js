4bbb075363f42bd01ec97ba338defed1c8845a6d
Nina Yeh <ninayeh1211@gmail.com>
2015-07-17 12:38:32 +0800

    socail plugin change

--- app/assets/javascripts/plugins/fb-setting.js
+++ app/assets/javascripts/plugins/fb-setting.js
@@ -1,3 +1,4 @@
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
 window.fbAsyncInit = function() {
     FB.init({
       appId      : $('body').data('facebook-appid'),
@@ -13,3 +14,13 @@ window.fbAsyncInit = function() {
      js.src = "//connect.facebook.net/en_US/sdk.js";
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));
+
+  (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4&appId=885911111489321";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+
