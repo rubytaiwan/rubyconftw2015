9cf82cc3f20238e1ccc8e1f6db07ea473f789d1d
Awaru Ryudo <ryudoawaru@gmail.com>
2015-07-26 23:54:25 +0800

    Fix migrate file error

--- db/migrate/20150725135223_add_dom_id_to_speakers.rb
+++ db/migrate/20150725135223_add_dom_id_to_speakers.rb
@@ -1,7 +1,6 @@
 class AddDomIdToSpeakers < ActiveRecord::Migration
   def up
     add_column :speakers, :dom_id, :string, limit: 32
-    Speaker.all.each{|s| s.dom_id = s.name.gsub(/\s/, '').underscore.dasherize;s.save;}
   end
 
   def down
