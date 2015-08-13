7a077ea516c3ffc3d9fbdb5df14014a1dd5b5f07
TinaHuang <abcyu108@gmail.com>
2015-08-13 16:09:17 +0800

    issue #59 scroll down fade in

--- app/assets/javascripts/fade_in.js
+++ app/assets/javascripts/fade_in.js
@@ -1,19 +1,3 @@
-//  /* Every time the window is scrolled ... */
-// $(window).scroll( function(){
-
-//     /* Check the location of each desired element */
-//     $('.fade_in').each( function(i){
-        
-//         var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-//         var bottom_of_window = $(window).scrollTop() + $(window).height();
-        
-//         /* If the object is completely visible in the window, fade it it */
-//         if( bottom_of_window > bottom_of_object ){
-//             $(this).animate({'opacity':'1'},500);        
-//         }  
-//     }); 
-// });
-
 $(window).scroll( function(){
     $('.fadeIn').each(function(i) {
         var bottom_of_object = $(this).offset().top + $(this).outerHeight() - 200 ;
