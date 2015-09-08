f9e5047ca5ba42b1273e86fca01c538a98e35ea1
Awaru Ryudo <ryudoawaru@gmail.com>
2015-09-08 11:56:42 +0800

    Fix js error on schedule page

--- app/assets/javascripts/plugins/fullNav.js
+++ app/assets/javascripts/plugins/fullNav.js
@@ -1,11 +1,12 @@
-document.querySelector( "#nav-toggle" )
-  .addEventListener( "click", function() {
-    $('#nav-toggle').toggleClass( 'active' );
-    $('.fullNavOverlay').toggleClass( 'full-active' );
+if($("#nav-toggle").length > 0){
+  document.querySelector( "#nav-toggle" )
+    .addEventListener( "click", function() {
+      $('#nav-toggle').toggleClass( 'active' );
+      $('.fullNavOverlay').toggleClass( 'full-active' );
 
-  $('.fullNavOverlay a').click(function() {
-    $('#nav-toggle').removeClass( "active" )
-    $('.fullNavOverlay').removeClass( "full-active" );
+    $('.fullNavOverlay a').click(function() {
+      $('#nav-toggle').removeClass( "active" )
+      $('.fullNavOverlay').removeClass( "full-active" );
+    });
   });
-});
-
+}
