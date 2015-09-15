dbf00e2893df2474cea9be189b33a28fe3b7be84
Awaru Ryudo <ryudoawaru@gmail.com>
2015-09-15 16:30:43 +0800

    use new deploy location

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 set :default_env, { path: "/usr/local/ruby22/bin:$PATH" }
 set :branch, "master"
 set :stage, :production
-set :deploy_to, '/home/deploy/2015.rubyconf.tw'
-role :web, "deploy@rubytw2.cloudapp.net"
-role :app, "deploy@rubytw2.cloudapp.net"
-role :db,  "deploy@rubytw2.cloudapp.net", :primary => true
+set :deploy_to, '/home/rubytw/2015.rubyconf.tw'
+role :web, "rubytw@60.250.121.194"
+role :app, "rubytw@60.250.121.194"
+role :db,  "rubytw@60.250.121.194", :primary => true
